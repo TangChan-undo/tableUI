6dc01369074da7cf5e2827d7be73bb0dc7b3a020
tc <1006077326@qq.com>
2023-12-27 23:58:06 +0800

    feat: save

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,22 @@
 // .vuepress/config.js
 module.exports = {
+  title: 'Table-UI',
   base: '/tableUI/',
   themeConfig: {
+    nav: [
+      {
+        text: 'HOME',
+        link: '/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/TangChan-undo/tableUI',
+      },
+      {
+        text: 'Npm',
+        link: 'https://www.npmjs.com/package/tc_table_ui',
+      },
+    ],
     sidebar: ['/', '/componentDocs/staticTable'],
   },
 }
